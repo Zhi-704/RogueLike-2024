b99e0c75e2d2f31861664e9c0edc4748a7b7f0ab
Zhi-704 <zhi.liang.704@gmail.com>
2024-08-06 15:26:51 +0100

    Player can now die

--- game_map.py
+++ game_map.py
@@ -81,8 +81,12 @@ class GameMap:
             choicelist=[self.tiles["light"], self.tiles["dark"]],
             default=tile_types.SHROUD,
         )
+        entities_sorted_for_rendering = sorted(
+            self.entities, key=lambda x: x.render_order.value
+        )
 
-        for entity in self.entities:
-            # Only print entities that are in the FOV
+        for entity in entities_sorted_for_rendering:
             if self.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
+                console.print(
+                    x=entity.x, y=entity.y, string=entity.char, fg=entity.color
+                )
